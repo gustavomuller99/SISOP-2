327cb60a046755ea5da0d70c9d5eb6f1d2c3ac58
Sofia Braga <sofiamsbraga@gmail.com>
2024-04-30 09:38:52 -0300

    starting monitoring service

--- src/roles/manager.cpp
+++ src/roles/manager.cpp
@@ -2,7 +2,7 @@
 
 void Manager::init() {
     this->init_discovery();
-    // this->init_monitoring();
+    this->init_monitoring();
     // this->init_management();
     // this->init_interface();
 }
@@ -46,6 +46,36 @@ void Manager::init_discovery() {
     close(this->sck_discovery);
 }
 
+void Manager::init_monitoring() {
+    // creating tcp server socket file descriptor
+    int trueflag = 1;
+    struct sockaddr_in recv_addr;
+
+    if ((this->sck_discovery = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+        exit(EXIT_FAILURE); 
+    
+    if (setsockopt(this->sck_discovery, SOL_SOCKET, SO_REUSEADDR, &trueflag, sizeof trueflag) < 0)
+        exit(EXIT_FAILURE); 
+
+    memset(&recv_addr, 0, sizeof recv_addr);
+
+    recv_addr.sin_family = AF_INET;
+    recv_addr.sin_port = htons(PORT_DISCOVERY); //ver port
+    recv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(this->sck_discovery, (struct sockaddr*) &recv_addr, sizeof recv_addr) < 0)
+        exit(EXIT_FAILURE); 
+
+    // Listen for incoming connections
+    if (listen(this->sck_discovery, SOMAXCONN) < 0)
+        exit(EXIT_FAILURE);
+
+    pthread_create(&this->t_discovery, NULL, Manager::monitoring, this);
+    pthread_join(this->t_discovery, NULL);
+
+    close(this->sck_discovery);
+}
+
 void *Manager::discovery(void *ctx) {
     Manager *m = ((Manager *) ctx);
 
@@ -67,3 +97,14 @@ void *Manager::discovery(void *ctx) {
     return 0;
 }
 
+void *Manager::monitoring(void *ctx) {
+    Manager *m = ((Manager *) ctx);
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    client_addr.sin_family = AF_INET;
+    
+    while (1) {
+        close(client_socket);
+    }
+}
