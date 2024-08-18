be02d83d6d11ce026d771076ed2e546487490860
Lucca Kroeff <luccafranke1@gmail.com>
2024-08-18 14:58:13 -0300

    fix

--- src/roles/host.cpp
+++ src/roles/host.cpp
@@ -414,6 +414,7 @@ void *Host::run_election(void *ctx) {
             for (auto replica_host: hosts_replica_c) {
                 // sends election message
                 if (stoi(replica_host.ip) > host_ip){
+                    std::cout<<"O MEU É MENOR";
 
                     // sends to PORT ELECTION, Host IP
                     addr.sin_port = (in_port_t) htons(PORT_ELECTION);
